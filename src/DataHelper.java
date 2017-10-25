652c5f225c119e976ba7ce752bb197f840a98ae7
李晓冬 <lixiao1k@163.com>
2017-10-25 23:01:12 +0800

    DataHelper makes the preparation.

---
+++ src/DataHelper.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by shelton on 2017/10/25.
+ */
+public class DataHelper {
+    public String initState[][];
+    public String keyworkState[][];
+    public String transState[][];
+
+    private String [] txturl = {"InitState.txt","KeyWordState.txt","TransState.txt"};
+
+    public DataHelper(){
+        setInitState();
+        setKeyworkState();
+        setTransState();
+    }
+
+    private void setInitState(){
+        ArrayList<String> datalist;
+        try {
+            datalist = readFile(txturl[0]);
+            int data_len = datalist.size();
+            initState = new String[data_len][3];
+            for(int i=0;i<data_len;i++){
+                String string_line = datalist.get(i);
+                String datas_line[] = string_line.split(" ");
+                for (int j=0;j<3;j++){
+                    initState[i][j] = datas_line[j];
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void setKeyworkState(){
+        ArrayList<String> datalist;
+        try {
+            datalist = readFile(txturl[1]);
+            int data_len = datalist.size();
+            keyworkState = new String[data_len][2];
+            for(int i=0;i<data_len;i++){
+                String string_line = datalist.get(i);
+                String datas_line[] = string_line.split(" ");
+                for(int j=0;j<2;j++){
+                    keyworkState[i][j] = datas_line[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setTransState(){
+        ArrayList<String> datalist;
+        try {
+            datalist = readFile(txturl[2]);
+            int data_len = datalist.size();
+            transState = new String[data_len][4];
+            for(int i=0;i<data_len;i++){
+                String string_line = datalist.get(i);
+                String datas_line[] = string_line.split(" ");
+                for (int j=0;j<4;j++){
+                    transState[i][j] = datas_line[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private ArrayList<String> readFile(String url) throws IOException {
+
+        ArrayList<String> datalist = new ArrayList<>();
+
+        File file = new File(url);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String readline = "";
+
+        while ((readline = bufferedReader.readLine())!=null){
+            datalist.add(readline);
+        }
+
+        bufferedReader.close();
+        fileReader.close();
+
+        return datalist;
+
+    }
+
+    public int isKeyWord(String s){
+        int keyword_nums = keyworkState.length;
+        for(int i=0;i<keyword_nums;i++){
+            if(s.equals(keyworkState[i][0])){
+                return Integer.parseInt(keyworkState[i][1]);
+            }
+        }
+        return -1;
+    }
+
+    public int isToken(char c){
+        int initState_nums = initState.length;
+        for(int i=0;i<initState_nums;i++){
+            char token = initState[i][0].charAt(0);
+            if(c == token){
+                return Integer.parseInt(initState[i][1]);
+            }
+        }
+        return -1;
+    }
+
+    public int transState(int state, char ch){
+        int transState_nums = transState.length;
+        for(int i=0;i<transState_nums;i++){
+            int oldstate = Integer.parseInt(transState[i][0]);
+            char nowchar = transState[i][1].charAt(0);
+            if(oldstate == state && nowchar == ch){
+                int nowState = Integer.parseInt(transState[i][2]);
+                return nowState;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
+
+}
---
+++ test/DataHelperTest.java
@@ -0,0 +1,66 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by shelton on 2017/10/25.
+ */
+
+public class DataHelperTest {
+    DataHelper dataHelper;
+    @Before
+    public void init(){
+        dataHelper = new DataHelper();
+    }
+    @Test
+    public void testdata(){
+        System.out.println("这是initState.txt的数据============================");
+        String[][] initState = dataHelper.initState;
+        print(initState);
+        System.out.println("这是keywordState.txt的数据=========================");
+        String[][] keywordState = dataHelper.keyworkState;
+        print(keywordState);
+        System.out.println("这是TransState.txt的数据===========================");
+        String[][] transState = dataHelper.transState;
+        print(transState);
+
+    }
+    @Test
+    public void testkeyword(){
+        assertEquals(60,dataHelper.isKeyWord("if"));
+        assertEquals(61,dataHelper.isKeyWord("else"));
+        assertEquals(99,dataHelper.isKeyWord("private"));
+        assertEquals(111,dataHelper.isKeyWord("volatile"));
+        assertEquals(-1,dataHelper.isKeyWord("huh"));
+
+    }
+
+    @Test
+    public void testToken(){
+        assertEquals(10,dataHelper.isToken('+'));
+        assertEquals(35,dataHelper.isToken('\"'));
+        assertEquals(-1,dataHelper.isToken('0'));
+    }
+
+    @Test
+    public void testTrans(){
+        assertEquals(48,dataHelper.transState(24,'='));
+        assertEquals(40,dataHelper.transState(10,'='));
+        assertEquals(54,dataHelper.transState(16,'<'));
+        assertEquals(44,dataHelper.transState(16,'='));
+        assertEquals(-1,dataHelper.transState(5,'f'));
+        assertEquals(-1,dataHelper.transState(10,'<'));
+    }
+
+
+    private void print(String[][] datalist){
+        int len1 = datalist.length;
+        int len2 = datalist[0].length;
+        for(int i=0;i<len1;i++) {
+            for (int j = 0; j < len2; j++) {
+                System.out.print(datalist[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("==================================================");
+    }
+}
